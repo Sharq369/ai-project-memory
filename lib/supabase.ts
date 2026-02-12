565df7762c15260d4ed1843b4f837156f0df7a82
SHARQ <140482475+Sharq369@users.noreply.github.com>
2026-02-12 06:38:25 +0100

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
+// These MUST have the NEXT_PUBLIC_ prefix to work in the login page
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
 
-// This is the exported name our login page will use
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
