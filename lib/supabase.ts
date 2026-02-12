e9a570dea9270b9e24395dfc253addde7e03a04c
SHARQ <140482475+Sharq369@users.noreply.github.com>
2026-02-12 05:56:33 +0100

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,4 +3,5 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
 
+// This is the exported name our login page will use
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
